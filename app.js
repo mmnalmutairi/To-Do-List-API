281cb6e09e566940d4c72aa55a90a74f51d2f24e
mahmood114 <mahmoodalmohri@gmail.com>
2021-07-04 00:58:22 +0300

    Added get, post, put and delete functions

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 const express = require("express");
-const data = require("./data");
-
+let data = require("./data");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/tasks", (req, res) => {
     res.json(data);
-});
+})
 
+app.post("/tasks", (req, res) => {
+    const task = {
+        id: data.length + 1,
+        ...req.body,
+        done: false,
+    }
+    data.push(task);
+    res.status(201).json({"message": `Data with ID ${task.id} has been successfully added.`});
+})
+
+app.put("/tasks/:taskId", (req, res) => {
+    const {taskId} = req.params;
+    const foundTask = data.find(task => task.id === +taskId)
+    
+    for(const key in req.body) foundTask[key] = req.body[key];
+    
+    res.json(data);
+
+})
+
+app.delete("/tasks/:taskId", (req, res) => {
+    const {taskId} = req.params;
+    data = data.filter(task => task.id !== +taskId);
+    
+    res.json(data);
+})
 
 app.listen(8000);
--- data.js
+++ data.js
@@ -1,18 +1,55 @@
 
 
-const data = [{
+// const data = [{
 
+//     id: 1,
+//     title: "Reading",
+//     description: " Read a specific book."
+// },
+// {
+
+//     id: 2,
+//     title: "Eating",
+//     description: " Eating a specific meal."
+// }
+
+// ]
+
+// module.exports = data;
+
+
+const data = [{
     id: 1,
     title: "Reading",
-    description: " Read a specific book."
+    description: " Read a specific book.",
+    done: false,
 },
 {
-
     id: 2,
     title: "Eating",
-    description: " Eating a specific meal."
+    description: " Eating a specific meal.",
+    done: false,
+},
+{
+    id: 3,
+    title: "Swimimg",
+    description: " Eating a specific meal.",
+    done: false,
+},
+{
+    id: 4,
+    title: "Riding",
+    description: " Eating a specific meal.",
+    done: false,
+},
+{
+    id: 5,
+    title: "Sleeping",
+    description: " Eating a specific meal.",
+    done: false,
 }
 
-]
+];
 
+// export default data;
 module.exports = data;
